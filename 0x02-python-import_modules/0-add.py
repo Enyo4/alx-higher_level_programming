7fc3cd21ea1948ff8c1a6111c9c924d8920c419a
Enyo4 <113777537+Enyo4@users.noreply.github.com>
2023-02-17 21:44:28 +0100

    Update 0-add.py
    comment for future use added

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """if __name__ == "__main__": allows you to execute code when the file runs as a script but not when imported as a module""" 
     """Print the sum of 1 and 2."""
     from add_0 import add
 
