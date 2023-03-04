cea324a95fdf4d2cfe519a291491e5bb7c44f8eb
Enyo4 <morakinyoelisha01@gmail.com>
2023-03-04 01:46:12 +0100

    correction of task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     """Replaces all occurrences of an element by another in a new list."""
+    new_list = []
     for num in my_list:
         if num == search:
             num = replace
-        new_list += num
+        new_list.append(num)
     return new_list
