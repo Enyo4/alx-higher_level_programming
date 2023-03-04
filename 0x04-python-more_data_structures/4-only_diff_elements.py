d99fe90098c97fe4820e6294104c677854089bd8
Enyo4 <morakinyoelisha01@gmail.com>
2023-03-04 03:27:53 +0100

    Edit

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 def only_diff_elements(set_1, set_2):
     """Returns a set of all elements present in only one set"""
-    return set(set_1) ^ set(set_2)
+    return set_1 ^ set_2
