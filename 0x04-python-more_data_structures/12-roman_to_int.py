dc0059594ec256626efe591f1b7a7971d58adf11
Enyo4 <morakinyoelisha01@gmail.com>
2023-03-05 23:17:47 +0100

    tested 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    romVal = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M":1000}
+    romVal = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     list = []
     n = 0
     for i in roman_string:
         if i not in romVal.keys():
             return None
         elif len(roman_string) == 1:
-           return romVal[i]
+            return romVal[i]
         else:
             list.append(i)
             n += int(romVal[i])
             for j in range(len(list)-1):
-                if list[j] == "I" and (list[j + 1] == "V" or list[j + 1] == "X"):
-                    n -= int(romVal[list[j]]* 2)
-                elif list[j] == "X" and (list[j + 1] == "L" or list[j + 1] == "C"):
+                a = list[j]
+                b = list[j + 1]
+                if a == "I" and (b == "V" or b == "X"):
                     n -= int(romVal[list[j]] * 2)
-                elif list[j] == "C" and (list[j + 1] == "D" or list[j + 1] == "M"):
+                elif a == "X" and (b == "L" or b == "C"):
+                    n -= int(romVal[list[j]] * 2)
+                elif a == "C" and (b == "D" or b == "M"):
                     n -= int(romVal[list[j]] * 2)
     return n
