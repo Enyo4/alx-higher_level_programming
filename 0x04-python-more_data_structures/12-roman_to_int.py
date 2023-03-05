af18a62a42f7827d150e68f322252cbab4169f14
Enyo4 <morakinyoelisha01@gmail.com>
2023-03-05 22:52:10 +0100

    Task 12 added

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    
+    romVal = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M":1000}
+    list = []
+    n = 0
+    for i in roman_string:
+        if i not in romVal.keys():
+            return None
+        elif len(roman_string) == 1:
+           return romVal[i]
+        else:
+            list.append(i)
+            n += int(romVal[i])
+            for j in range(len(list)-1):
+                if list[j] == "I" and (list[j + 1] == "V" or list[j + 1] == "X"):
+                    n -= int(romVal[list[j]] * 2)
+    return n
