6d19a9d3e74703accbb461fdfb93fa0c406c8412
Eniola <morakinyoelisha01@gmail.com>
2024-03-14 21:59:42 -0700

    prints number and args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    
+    total = int(len(sys.argv)) - 1
+    if total == 1:
+        print("1 argument")
+    else:
+        print("{} arguments".format(total))
+
+    for i in range(1, total + 1):
+        print("{}: {}".format(i, sys.argv[i]))
