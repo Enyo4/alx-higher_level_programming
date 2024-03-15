d25d11ffe3f0214f19dd00e0127c39c57be17b1d
Eniola <morakinyoelisha01@gmail.com>
2024-03-14 22:33:34 -0700

    Few changes made

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     total = len(sys.argv) - 1
     if total == 0:
-        print("0 argument.")
+        print("0 arguments.")
     elif total == 1:
         print("{} argument:".format(total))
     else:
