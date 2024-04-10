c65a60bbbc6d2f13e584d118e8ff848735e9eb1c
Eniola <morakinyoelisha01@gmail.com>
2024-04-10 15:41:23 -0700

    Prints an integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """Prints an integer with "{:d}".format().
+
+    Args:
+        value: Can be of any type
+
+    Returns:
+        True if Value has been correctly printed, Otherwise false
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except ValueError:
+        return (False)
