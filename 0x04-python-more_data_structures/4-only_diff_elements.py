af0db3317cc10dbc04c404b6a273d4e64787a000
Eniola <morakinyoelisha01@gmail.com>
2024-04-15 13:43:59 -0700

    Compare sets

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    """Returns a set of all elements present in only  one set.
 
+    Args:
+        set_1: The first set given
+        set_2: The second given set
+
+    Returns: A new set of elements in only one set_1 or set_2 but not in both
+    """
+    return set_1 ^ set_2
