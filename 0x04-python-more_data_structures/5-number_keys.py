4f0a6f288d7f0e30996f238144cd61587de1088d
Eniola <morakinyoelisha01@gmail.com>
2024-04-15 13:59:26 -0700

    Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """Returns the number of keys in a dictionary.
+
+    Args:
+        @a_dictionary: A dictionary
+
+    Return: Number of keys
+    """
+    return (len(a_dictionary.keys()))
