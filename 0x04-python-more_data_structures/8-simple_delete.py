10b865d84a2c07eaa5cec71c81167999fe3a844d
Eniola <morakinyoelisha01@gmail.com>
2024-04-16 06:37:14 -0700

    Deletes key

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    """Deletes a key in a dictionary
+
+    Args:
+        a_dictionary: Given dictioinary
+        key: The dictionary key to delete
+
+    Returns: The updated dictionary
+    """
+    if key in a_dictionary:
+        del(a_dictionary[key])
+
+    return (a_dictionary)
