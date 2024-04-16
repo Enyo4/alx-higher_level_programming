bdcb1b8f6664c2f2efe805e19684e55edd667e81
Eniola <morakinyoelisha01@gmail.com>
2024-04-16 16:34:16 -0700

    ?

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+    """Returns a list with all values mutiplied by a number without any loops
+
+    Args:
+        my_list: The input list
+        number: number each values should be multiplied by
+
+    Returns: A new list same length as my_list
+    """
