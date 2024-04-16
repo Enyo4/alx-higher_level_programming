886159547109b2d3949dcc0d91b22bce435993d9
Eniola <morakinyoelisha01@gmail.com>
2024-04-16 06:41:20 -0700

    Checker test

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -9,6 +9,6 @@ def simple_delete(a_dictionary, key=""):
     Returns: The updated dictionary
     """
     if key is not None and key in a_dictionary:
-        del(a_dictionary[key])
+        del a_dictionary[key]
 
     return (a_dictionary)
