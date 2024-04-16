61ef5d40e7cb4482bfba66ec842c36e5dacd5cd4
Eniola <morakinyoelisha01@gmail.com>
2024-04-16 06:08:03 -0700

    Trying to get it right

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,11 +2,8 @@ def print_sorted_dictionary(a_dictionary):
     """ Prints a dictionary by ordered keys.
 
     Args:
-        @a_dictionary: The given dictionary
+        a_dictionary: The given dictionary
 
-    Returns: Sorted dictionary
     """
-    sub_list = list(a_dictionary.keys())
-    sub_list.sort()
-    for i in sub_list:
-        print("{}: {}".format(i, a_dictionary[i]))
+    for key in sorted(a_dictionary.keys()):
+        print("{}: {}".format(key, a_dictionary[key]))
