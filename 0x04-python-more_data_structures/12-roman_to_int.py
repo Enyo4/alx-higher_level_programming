88bd675b17f5e6b449399f1b561dc6fc407a56f0
Eniola <morakinyoelisha01@gmail.com>
2024-04-17 11:15:29 -0700

    Try 1

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,10 +9,7 @@ def roman_to_int(roman_string):
     """
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
-
-    if roman_string is None:
-        return 0
-
+    
     for s in roman_string:
         if s not in rom_val:
             return 0
