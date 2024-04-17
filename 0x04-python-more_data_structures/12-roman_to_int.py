dbe862fb8a1c86c05d75acff455be005eb3ef996
Eniola <morakinyoelisha01@gmail.com>
2024-04-17 11:38:15 -0700

    pychecker passed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,7 +9,7 @@ def roman_to_int(roman_string):
     """
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
-    
+
     if not isinstance(roman_string, str) or roman_string == "":
         return 0
 
