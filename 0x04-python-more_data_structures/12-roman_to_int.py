b29fd71ca73d1393818e90fd86113b36e573205a
Eniola <morakinyoelisha01@gmail.com>
2024-04-17 11:19:37 -0700

    Added test case to know if char

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,6 +10,9 @@ def roman_to_int(roman_string):
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     
+    if not isinstance(roman_string, str) or roman_string == "":
+        return None
+
     for s in roman_string:
         if s not in rom_val:
             return 0
